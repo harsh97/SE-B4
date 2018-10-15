822e43e5e57ca5885068a13b01bade52781c267d
Gunashree R V <gunashree24@gmail.com>
2018-10-15 22:43:27 +0530

    Minor bug fix

---
+++ public/js/userTripUpdate.js
@@ -0,0 +1,17 @@
+$('#CancelTrip').click(() => {
+    Metro.dialog.create({
+    title: "Cancel Trip",
+    content: "<div>Are you sure you want to cancel this trip ?</div>",
+    actions: 
+    [
+        {
+            caption: "Yes",
+            cls: "js-dialog-close alert",
+        },
+        {
+            caption: "No",
+            cls: "js-dialog-close",
+        }
+    ]
+    });
+});
