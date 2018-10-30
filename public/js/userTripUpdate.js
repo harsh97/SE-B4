2db575ebc29c4de63204250aa66594e9c0b780a8
Gunashree R V <gunashree24@gmail.com>
2018-10-30 10:11:29 +0530

    Added onload

--- public/js/userTripUpdate.js
+++ public/js/userTripUpdate.js
@@ -1,17 +1,19 @@
-$('#CancelTrip').click(() => {
-    Metro.dialog.create({
-    title: "Cancel Trip",
-    content: "<div>Are you sure you want to cancel this trip ?</div>",
-    actions: 
-    [
-        {
-            caption: "Yes",
-            cls: "js-dialog-close alert",
-        },
-        {
-            caption: "No",
-            cls: "js-dialog-close",
-        }
-    ]
+$(document).ready(function(){
+    $('#CancelTrip').click(() => {
+        Metro.dialog.create({
+        title: "Cancel Trip",
+        content: "<div>Are you sure you want to cancel this trip ?</div>",
+        actions: 
+        [
+            {
+                caption: "Yes",
+                cls: "js-dialog-close alert",
+            },
+            {
+                caption: "No",
+                cls: "js-dialog-close",
+            }
+        ]
+        });
     });
 });
