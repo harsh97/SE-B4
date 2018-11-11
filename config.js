b62e764426cff653b1a0f15160762589bad48a10
Gunashree R V <gunashree24@gmail.com>
2018-11-11 14:09:57 +0530

    Minor changes in leafletMap

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var path = require('path');
 var config = {
 		host: 'localhost',
 		user: 'postgres',
-		password: 'yashu',
-		port: 5432,
+		password: 'qmpzal',
+		port: 5433,
 		database: 'transport_management_system',
 		max: '10',
 		idleTimeoutMillis: 100 // how long a client is allowed to remain idle before being closed
