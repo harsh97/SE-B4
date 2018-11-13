0f5e8a56ffa733ae58ae1d6811d12a789c0dcc71
Gunashree R V <gunashree24@gmail.com>
2018-11-13 20:09:01 +0530

    Added required packages

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ var path = require('path');
 var config = {
 		host: 'localhost',
 		user: 'postgres',
-		password: 'a',
-		port: 5432,
-		database: 'transport_management_system',
+		password: 'qmpzal',
+		port: 5433,
+		database: 'tms',
 		max: '10',
 		idleTimeoutMillis: 100 // how long a client is allowed to remain idle before being closed
 }
